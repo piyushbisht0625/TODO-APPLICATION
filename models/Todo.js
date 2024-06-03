2d5470a0a21e7bc6cc114ca85b4c93dcdd444dea
Piyush Bisht <piyushbisht0625@gmail.com>
2024-06-03 12:52:03 +0530

    Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -4,7 +4,7 @@ const todoSchema = new mongoose.Schema(
     {
         title:{
             type:String,
-            required:true,
+            required:true, 
             maxLength:50,
         }, 
          
